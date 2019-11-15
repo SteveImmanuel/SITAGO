e9ae9efe259b87323194a98f240b0903b3d4c3b1
jrandiny <jrandiny2@gmail.com>
2019-11-15 17:57:37 +0700

    Update face to use class

--- face.py
+++ face.py
@@ -14,58 +14,57 @@ class FaceData():
     face_names: List[str]
 
 
-def loadFace():
-    file_list = os.listdir(FACE_DIRECTORY)
+class faceParser():
+    def __init__(self, *args):
+        if len(args) == 1:
+            self.loadFace(args[0])
+        else:
+            self.loadFace(FACE_DIRECTORY)
 
-    face_file_list = [f for f in file_list if f.endswith('.face')]
+    def loadFace(self, face_directory: str) -> None:
+        file_list = os.listdir(face_directory)
 
-    face_data_list = [
-        np.load(os.path.join(FACE_DIRECTORY, filename)) for filename in face_file_list
-    ]
-    face_name_list = [filename[:-5] for filename in face_file_list]
+        face_file_list = [f for f in file_list if f.endswith('.face')]
 
-    return FaceData(face_data_list, face_name_list)
+        face_data_list = [
+            np.load(os.path.join(FACE_DIRECTORY, filename)) for filename in face_file_list
+        ]
+        face_name_list = [filename[:-5] for filename in face_file_list]
 
+        self.face_data = FaceData(face_data_list, face_name_list)
 
-def createFaceData(image_path: str, name: str):
-    picture = face_recognition.load_image_file(image_path)
-    face_encoding = face_recognition.face_encodings(picture)[0]
+    def createFaceData(self, image_path: str, name: str) -> None:
+        picture = face_recognition.load_image_file(image_path)
+        face_encoding = face_recognition.face_encodings(picture)[0]
 
-    npy_dir = os.path.join(FACE_DIRECTORY, '{}.npy'.format(name))
-    face_dir = os.path.join(FACE_DIRECTORY, '{}.face'.format(name))
+        npy_dir = os.path.join(FACE_DIRECTORY, '{}.npy'.format(name))
+        face_dir = os.path.join(FACE_DIRECTORY, '{}.face'.format(name))
 
-    np.save(npy_dir, face_encoding)
+        np.save(npy_dir, face_encoding)
 
-    os.rename(npy_dir, face_dir)
+        os.rename(npy_dir, face_dir)
 
+    def testFace(self, image_path: str) -> str:
+        face_features = self.face_data.face_features
+        face_names = self.face_data.face_names
 
-def testFace(image_path: str):
-    face_features = []
-    face_names = []
+        test_picture = face_recognition.load_image_file(image_path)
+        test_feature = face_recognition.face_encodings(test_picture)[0]
 
-    # Load face data
-    if os.path.isdir(FACE_DIRECTORY):
-        face_data = loadFace()
-        face_features = face_data.face_features
-        face_names = face_data.face_names
-    else:
-        raise Exception('No face data found')
+        result_list = face_recognition.compare_faces(face_features, test_feature)
 
-    test_picture = face_recognition.load_image_file(image_path)
-    test_feature = face_recognition.face_encodings(test_picture)[0]
+        result_face_index = [i for i, result in enumerate(result_list) if result]
 
-    result_list = face_recognition.compare_faces(face_features, test_feature)
-
-    result_face_index = [i for i, result in enumerate(result_list) if result]
-
-    if len(result_face_index) >= 1:
-        return face_names[result_face_index[0]]
-    else:
-        return None
+        if len(result_face_index) >= 1:
+            return face_names[result_face_index[0]]
+        else:
+            return None
 
 
 if __name__ == '__main__':
     try:
+        parser = faceParser()
+
         if len(sys.argv) < 2:
             raise Exception('Usage: python face.py <image to test>')
 
@@ -73,11 +72,11 @@ if __name__ == '__main__':
             if len(sys.argv) < 4:
                 raise Exception('Usage: python face.py enroll <image> <name>')
             else:
-                createFaceData(sys.argv[2], sys.argv[3])
+                parser.createFaceData(sys.argv[2], sys.argv[3])
         else:
             file_test = sys.argv[1]
             if os.path.isfile(file_test):
-                print("Found : {}".format(testFace(file_test)))
+                print("Found : {}".format(parser.testFace(file_test)))
             else:
                 raise Exception('Test file not found')
     except Exception as err:
